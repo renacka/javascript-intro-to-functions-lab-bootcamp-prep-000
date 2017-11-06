ebc000bea819e65f82fa7d5a1e241bcf31f2ecac
NAk BURGESS <nakburgess7@gamil.com>
2017-11-06 04:57:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 
+  function shout(string) {
+    return string.toUpperCase()
+}
+
+
+  function whisper(string) {
+  return string.toLowerCase()
+}
+
+    console.log.restore()
+  })
+
+    function logShout(string){
+      console.log('HELLO')
+    }
+
+    console.log.restore()
+  })
+
+  function logWhisper(string){
+    console.log('hello')
+  }
+
+  function sayHiToGrandma(string){
+    if (string === string.toLowerCase()){
+    var lowerCase = string.toLowerCase();
+      return ('I can\'t hear you!');
+
+}
+
+    if (string === string.toUpperCase()){
+      var upperCase = string.toUpperCase();
+      return 'YES INDEED!';
+        }
+
+
+
+    if (string === 'I love you, Grandma.'){
+
+          return 'I love you, too.';
+        }
